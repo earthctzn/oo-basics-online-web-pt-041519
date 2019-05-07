30e5ed1c9e2ef9e46081b696c09890d6924194bb
Caleb Aviles <earthctzn1@gmail.com>
2019-05-07 03:09:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -16,4 +16,28 @@ class Shoe
   def color
     @color
   end
+  
+  def size= (size)
+    @size = size
+  end
+  
+  def size
+    @size
+  end
+  
+  def material= (material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition= (condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
 end
